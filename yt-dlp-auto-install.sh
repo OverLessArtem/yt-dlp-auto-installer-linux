138a096c846785034c2145145506622a9f021067
OverLessArtem <artemfreedom@tuta.io>
2025-07-28 11:51:17 +0500

    Added doas support and changed wget to curl

--- yt-dlp-auto-install.sh
+++ yt-dlp-auto-install.sh
@@ -2,11 +2,25 @@
 # Script to install yt-dlp to /usr/local/bin
 # By ArtemOver
 
-# Download the latest version of yt-dlp
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O yt-dlp
+if command -v sudo &> /dev/null; then
+    SU="sudo"
+elif command -v doas &> /dev/null; then
+    SU="doas"
+else
+    echo "Error: neither sudo nor doas is installed. Cannot continue" >&2
+    exit 1
+fi
+
+if ! command -v curl &> /dev/null; then
+    echo "Error: 'curl' is not installed. Cannot download yt-dlp" >&2
+    exit 1
+fi
+
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 
 chmod +x yt-dlp
 
-sudo mv yt-dlp /usr/local/bin/yt-dlp
+$SU mv yt-dlp /usr/local/bin/yt-dlp
 
+# Show installed version
 echo "Installed yt-dlp version: $(yt-dlp --version)"
